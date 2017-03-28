438df480ecb6533c97737caba30ceb771493ebfb
MamaShip <youdangls@gmail.com>
2017-03-28 09:59:29 +0800

    注释

--- robot.py
+++ robot.py
@@ -13,15 +13,18 @@ import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
+#从imageUrls.txt读取图片列表
 file = open("/root/bot/pic/imageUrls.txt")
 image_list = []
 for line in file:
     image_list.append(line.strip('\n'))
 n = len(image_list)
+
 #bot_name = '@Twisted_bot'
 #global bot
 #bot = telegram.Bot(token='362488155:AAHrtcYCxucyZCHA4gcvJwGTZFfwRSUHjWM')
 
+#定义各项命令对应的操作
 def start(bot,update):
     bot.sendMessage(chat_id=update.message.chat_id, text="我是游荡的机器人(๑• . •๑)\n @MamaShip 锐意制作中！\n请使用/help 查看可用命令")
 def echo(bot, update):
@@ -42,7 +45,6 @@ def test(bot,update):
     print update
     print type(update)
 
-
 def help(bot,update):
     text = ('/start - 说实话，我不知道你用这个命令能干啥\n'
             '/yo - 跟我打个招呼吧\n'
@@ -50,7 +52,7 @@ def help(bot,update):
             '/help - 就是你在看的这个\n')
     bot.sendMessage(chat_id=update.message.chat_id, text=text)
 
-
+#为对应命令添加句柄到dispatcher
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
@@ -62,7 +64,6 @@ dispatcher.add_handler(cat_handler)
 
 echo_handler = MessageHandler(Filters.text, echo)
 dispatcher.add_handler(echo_handler)
-
 
 
 #try yo
@@ -81,7 +82,7 @@ dispatcher.add_handler(unknown_handler)
 * ／family 查看自己的家谱
 '''
 
+#开始接收数据
 updater.start_polling()
-
-
+#提供结束程序的操作
 updater.idle()
